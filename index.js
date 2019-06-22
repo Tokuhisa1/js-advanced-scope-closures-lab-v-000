57cadc1bc059574bd2bab983ce58f1c7cd648e5a
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 22:48:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// produceDrivingRange() - Calculates whether a given trip is within
-// range. For example, "produceDrivingRange(10)" returns a function
-// that will take two strings as arguments and returns a message
-// stating whether the trip is in range. If foo =
+// produceDrivingRange() - Calculates whether a given trip is
+// within range. For example, "produceDrivingRange(10)" returns a
+// function that will take two strings as arguments and returns a
+// message stating whether the trip is in range. If foo =
 // produceDrivingRange(10), then foo('12th', '15th') would return
 // "within range by 7" and foo('12th', '30th') would return "8
 // blocks out of range". We recommend referencing the
@@ -9,9 +9,9 @@
 function produceDrivingRange() {
 
 }
-// produceTipCalculator() - Returns a function that then calculates
-// a tip. For example, produceTipCalculator(.10) returns a function
-// that calculates ten percent tip on a fare.
+// produceTipCalculator() - Returns a function that then
+// calculates a tip. For example, produceTipCalculator(.10)
+// returns a function that calculates ten percent tip on a fare.
 // produceTipCalculator(.20) returns a function that calculates
 // twenty percent tip on a fare.
 function produceTipCalculator() {
@@ -22,5 +22,5 @@ function produceTipCalculator() {
 // a new driver is created. The rest of the code base does not have
 // access to driverId.
 function createDriver() {
-  
+
 }
