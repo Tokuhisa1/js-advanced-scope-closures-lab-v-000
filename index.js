cf2d6263019cc0002f4de68717385f66d5b857d6
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 22:48:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function produceTipCalculator() {
 }
 // createDriver is a function that returns a Driver class. The
 // class has reference to a driverId that is incremented each time
-// a new driver is created. The rest of the code base does not have
-// access to driverId.
+// a new driver is created. The rest of the code base does not
+// have access to driverId.
 function createDriver() {
 
 }
